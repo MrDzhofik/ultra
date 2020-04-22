45e16f2e36cfcb89fba3fb5534d03ba4288b084e
MrDzhofik <60004361+MrDzhofik@users.noreply.github.com>
2020-04-22 21:31:42 +0300

    Hello

--- app/models.py
+++ app/models.py
@@ -4,7 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
-import dns.resolver
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 
 
 class User(UserMixin, db.Model):
@@ -13,6 +19,14 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
